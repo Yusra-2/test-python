4e490f36c8b5569f99195befa7fdd292d038df80
unknown <yusra.alghilani2@gmail.com>
2023-11-14 16:32:44 +0400

    updated

--- ex2.py
+++ ex2.py
@@ -3,8 +3,9 @@ block_w= 5
 rest_blocks= wall - block_w
 pairs= 2* block_w
 t_pairs= int(rest_blocks / pairs)
-gap= block_w / 2
+t_gap= (9%2)*5
 print(rest_blocks)
 print(pairs)
 print(t_pairs)
-print(gap)
+print(t_gap)
+
