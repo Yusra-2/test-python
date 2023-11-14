c95811d7636d412ab211a5456237cc7c955aea28
unknown <yusra.alghilani2@gmail.com>
2023-11-14 16:18:27 +0400

    first commit

---
+++ ex2.py
@@ -0,0 +1,10 @@
+wall= 100
+block_w= 5
+rest_blocks= wall - block_w
+pairs= 2* block_w
+t_pairs= int(rest_blocks / pairs)
+gap= block_w / 2
+print(rest_blocks)
+print(pairs)
+print(t_pairs)
+print(gap)
